618c635048cebc6e3f4c4f6d052c39c36786d2a1
Mamadou Samba <mamadou.samba@starlims.com>
2022-06-22 11:03:15 +0200

    erreur strem permeission storage

--- sh/laravel/laravel.local.sh
+++ sh/laravel/laravel.local.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# erreur stream : https://stackoverflow.com/questions/64551170/laravel-the-stream-or-file-storage-laravel-log-could-not-be-opened-in-append-m
+chown -R www-data:www-data ./storage
+find ./storage -type d -exec chmod 775 {} \;
+find ./storage -type f -exec chmod 664 {} \;
+sudo chown www-data:www-data ./bootstrap/cache -R
+find ./bootstrap/cache -type d -exec chmod 775 {} \;
+chown -R www-data:www-data .env
+find .env -type d -exec chmod 775 {} \;
+
 # initialize laravel
 cd /var/www/src
 composer install --optimize-autoloader
